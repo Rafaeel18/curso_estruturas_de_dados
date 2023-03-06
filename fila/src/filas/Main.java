b36bf4a6e6d7ad692c5495e43c83687b90975844
Rafael <112227671+Rafaeel18@users.noreply.github.com>
2023-03-05 22:19:57 -0300

    Add files via upload

---
+++ fila/src/filas/Main.java
@@ -0,0 +1,34 @@
+package filas;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Fila<String>  minhaFila = new Fila<>();
+		
+		minhaFila.enqueue("Primeiro");
+		minhaFila.enqueue("segundo");
+		minhaFila.enqueue("terceiro");
+		minhaFila.enqueue("Cuarto");
+		
+		System.out.println(minhaFila);
+		
+		
+		
+		System.out.println(minhaFila.dequeue());
+		
+		System.out.println(minhaFila);
+		
+		minhaFila.enqueue("ultimo");
+
+		System.out.println(minhaFila);
+		
+		
+		System.out.println(minhaFila.first());
+		
+		System.out.println(minhaFila);
+		
+	
+	}
+
+}
